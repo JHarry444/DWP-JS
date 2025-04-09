55d945d13be21037bee4b41f2e81de0254c0003c
jharry444 <jharrison1@qa.com>
2025-04-09 15:56:46 +0100

    added truthy falsey

--- 03-Flow_Of_Control/index.js
+++ 03-Flow_Of_Control/index.js
@@ -1,18 +1,29 @@
 debugger;
-let num = prompt("Enter a whole number:");
-// condition
+// prompt() returns a string parseInt() converts a string into a number 
+let num = parseInt(prompt("Enter a whole number:"));
+// conditionals
 
-if (num % 1 > 0) {
+// if (num === "" || num === 0 || num === null || num === undefined || num === NaN)
+
+if (!num) {
+    alert("Invalid number: " + num)
+} else if (num % 1 > 0) {
     alert("Not a whole number!");
     alert("Next time enter a whole number!");
 } else if (num % 2 === 0) {
     alert("That number is even!")
 } else if (num % 2 === 1) {
     alert("That number is odd!")
-} else {
-    alert("Not A Number!");
 }
 
-if (num % 2 == 0 && num > `0`) {
-    alert("Greater than 10 AND a whole number!");
-}
+// if(variable) {
+//     do thing
+// }
+
+// if (num % 2 == 0 && num > `0`) {
+//     alert("Greater than 10 AND a whole number!");
+// }
+
+alert(num % 2 === 0 ? "Is even" : "Is odd");
+
+// loops
